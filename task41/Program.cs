21b7c075dc3368b9db0112d8d6d9409d38a8f353
Evgeniya <prahova.e@ya.ru>
2023-02-21 01:23:03 +0500

    all tasks

---
+++ task41/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+System.Console.WriteLine("Введите число: ");
+int[] array = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+
+int countNumbers(int[] array)
+{
+    int result = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(array[i] > 0)
+        result++;
+    }
+    return result;
+}
+void PrintArray(int[] array)
+{
+    System.Console.WriteLine("[" + string.Join(", ", array) + "]");
+}
+
+PrintArray(array);
+System.Console.WriteLine($"Введено чисел больше 0 = {countNumbers(array)}");
---
+++ task43/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+
+double InputNumber(string text)
+{
+    System.Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+
+
+
+
+double k1 = InputNumber("Введите значение k1: ");
+double b1 = InputNumber("Введите значение b1: ");
+double k2 = InputNumber("Введите значение k2: ");
+double b2 = InputNumber("Введите значение b2: ");
